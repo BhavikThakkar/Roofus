d825355672c58efce953633d4556939b670f3fd5
hardik.thakkar_techextensor <hardik.thakkar@techextensor.com>
2022-04-14 16:55:58 +0530

    First commit

---
+++ Roofus/src/pages/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+    <h1> Login Page </h1>
+    <q-btn color="white" text-color="black" label="Login" @click="onLoginButtonClick" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'LoginPage',
+  methods: {
+    onLoginButtonClick() {
+      console.log('onLoginButtonClick');
+    }
+  }
+})
+</script>
